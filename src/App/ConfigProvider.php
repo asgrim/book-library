8ee7f7bd74f28e99b6be6dab528dcf20dd1e408b
James Titcumb <james@asgrim.com>
2018-03-07 12:51:18 +0000

    Moved all of Doctrine's config into app config

--- src/App/ConfigProvider.php
+++ src/App/ConfigProvider.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace App;
 
 use ContainerInteropDoctrine\EntityManagerFactory;
+use Doctrine\Common\Persistence\Mapping\Driver\MappingDriverChain;
+use Doctrine\DBAL\Driver\PDOPgSql\Driver;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 use PSR7Sessions\Storageless\Http\SessionMiddleware;
 use Zend\ServiceManager\Factory\InvokableFactory;
 
@@ -15,13 +18,11 @@ final class ConfigProvider
     {
         return [
             'dependencies' => $this->getDependencies(),
+            'doctrine' => $this->getDoctrine(),
         ];
     }
 
-    /**
-     * Returns the container dependencies
-     */
-    public function getDependencies() : array
+    private function getDependencies() : array
     {
         return [
             'factories'  => [
@@ -33,6 +34,35 @@ final class ConfigProvider
                 Service\Book\FindBookByUuidInterface::class => Service\Book\DoctrineFindBookByUuidFactory::class,
                 SessionMiddleware::class => Middleware\SessionMiddlewareFactory::class,
                 Middleware\FlushingMiddleware::class => Middleware\FlushingMiddlewareFactory::class,
+            ],
+        ];
+    }
+
+    private function getDoctrine(): array
+    {
+        return [
+            'connection' => [
+                'orm_default' => [
+                    'driver_class' => Driver::class,
+                    'params' => [
+                        'url' => 'configure this in local.php',
+                    ],
+                ],
+            ],
+            'driver' => [
+                'orm_default' => [
+                    'class' => MappingDriverChain::class,
+                    'drivers' => [
+                        'App\Entity' => 'app_entity',
+                    ],
+                ],
+                'app_entity' => [
+                    'class' => AnnotationDriver::class,
+                    'cache' => 'array',
+                    'paths' => [
+                        __DIR__ . '/../../src/App/Entity',
+                    ],
+                ],
             ],
         ];
     }
